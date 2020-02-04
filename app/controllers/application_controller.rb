fe13374379020b8eab6ba607c60ff3885a7bcffb
Kengo <funeace@gmail.com>
2020-02-04 11:42:37 +0900

    [Progress]Bookers_041142

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
   end
+
+  #ログイン後のリダイレクト先を明示的に変更する　pathを指定することで、変更できる
+  def after_sign_in_path_for(resource)
+    user_path(current_user.id)
+  end
 end
